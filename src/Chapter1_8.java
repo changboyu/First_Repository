9cc34dac30e97d7bc8e36372c129bf1b8e1b04cd
changboyu <chang.boyu88@gmail.com>
2014-07-09 17:27:31 +1000

    1.8

--- src/Chapter1_8.java
+++ src/Chapter1_8.java
@@ -1,19 +1,29 @@
 public class Chapter1_8 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	}
+		public static void main(String[] args) {
+			String str1 = "waterbottle";
+			String str2 = "erbottlewat";
 
-	public boolean isSubstring(String str1, String str2) {
-		return true;
-	}
+			System.out.println(isRotation(str1, str2));
 
-	public boolean isRotation(String str1, String str2) {
+		}
+
+		public static boolean isSubstring(String big, String small) {
+			if (big.indexOf(small) >= 0) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+
+	public static boolean isRotation(String str1, String str2) {
 		int len = str1.length();
 		if (len == str2.length() && len > 0) {
 			String str1str1 = str1 + str1;
-			return isSubstring(str2,str1str1);
+			
+			return isSubstring(str1str1,str2);
 		}
 		return false;
 	}
